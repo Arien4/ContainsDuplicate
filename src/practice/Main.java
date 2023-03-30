b1bba9f8b96fd1274986499f5fb75a7a27f28557
Olena Zaviriukha <olenazaviriukha@gmail.com>
2023-03-30 10:30:11 +0300

    Initial commit

---
+++ src/practice/Main.java
@@ -0,0 +1,29 @@
+package practice;
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+        for (int num: nums) {
+            if (!numsSet.contains(num)) numsSet.add(num);
+            else return true;
+        }
+        return false;
+    }
+}
